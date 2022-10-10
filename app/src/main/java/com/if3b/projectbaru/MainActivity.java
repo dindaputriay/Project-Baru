0c21210773601a045fb9ca3570ae2e052d013fbb
dindaputriay <dindaputri@mhs.mdp.ac.id>
2022-10-10 11:31:16 +0700

    project baru

---
+++ app/src/main/java/com/if3b/projectbaru/MainActivity.java
@@ -0,0 +1,26 @@
+package com.if3b.projectbaru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+Button btnButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnButton =(Button)findViewById(R.id.btn_Button);
+
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Toast.makeText(getApplicationContext(), "Apa Kabar Kamu", Toast.LENGTH_LONG).show();
+            }
+        });
+        }
+    }
